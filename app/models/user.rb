6bb77890274d43605476df706d0b8ebe8569a9f0
sonnytrue <jericbello@gmail.com>
2010-08-23 14:49:20 +0800

    Secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
 EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -7,5 +8,21 @@ EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates_length_of :name, :maximum => 50
   validates_format_of :email, :with => EmailRegex
   validates_uniqueness_of :email, :case_sensitive => false
+  validates_confirmation_of :password
+
+  validates_presence_of :password
+  validates_length_of :password, :within => 6..40
+
+before_save :encrypt_password
+
+  private
+
+    def encrypt_password
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      string # Only a temporary implementation!
+    end
 
 end
