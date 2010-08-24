27c47688cf9ea085de40841117b1c33aa73a13d2
jeric <jericbello@gmail.com>
2010-08-24 16:37:15 +0800

    User model and page but not linked

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100823064124) do
+ActiveRecord::Schema.define(:version => 20100824061958) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(:version => 20100823064124) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,47 +14,47 @@ describe User do
     User.create!(@attr)
   end
 
-it "should require a name" do
-no_name_user = User.new(@attr.merge(:name => ""))
-no_name_user.should_not be_valid
-end
+  it "should require a name" do
+    no_name_user = User.new(@attr.merge(:name => ""))
+    no_name_user.should_not be_valid
+  end
 
-it "should reject names that are too long" do
-long_name = "a" * 51
-long_name_user = User.new(@attr.merge(:name => long_name))
-long_name_user.should_not be_valid
-end
+  it "should reject names that are too long" do
+    long_name = "a" * 51
+    long_name_user = User.new(@attr.merge(:name => long_name))
+    long_name_user.should_not be_valid
+  end
 
-it "should accept valid email addresses" do
-addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
-addresses.each do |address|
-valid_email_user = User.new(@attr.merge(:email => address))
-valid_email_user.should be_valid
-end
-end
-it "should reject invalid email addresses" do
-addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
-addresses.each do |address|
-invalid_email_user = User.new(@attr.merge(:email => address))
-invalid_email_user.should_not be_valid
-end
-end
+  it "should accept valid email addresses" do
+    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+    addresses.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.should be_valid
+    end
+  end
+  it "should reject invalid email addresses" do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+    addresses.each do |address|
+      invalid_email_user = User.new(@attr.merge(:email => address))
+      invalid_email_user.should_not be_valid
+    end
+  end
 
-it "should reject duplicate email addresses" do
-# Put a user with given email address into the database.
-User.create!(@attr)
-user_with_duplicate_email = User.new(@attr)
-user_with_duplicate_email.should_not be_valid
-end
+  it "should reject duplicate email addresses" do
+    # Put a user with given email address into the database.
+    User.create!(@attr)
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
 
-it "should reject email addresses identical up to case" do
-upcased_email = @attr[:email].upcase
-User.create!(@attr.merge(:email => upcased_email))
-user_with_duplicate_email = User.new(@attr)
-user_with_duplicate_email.should_not be_valid
-end
+  it "should reject email addresses identical up to case" do
+    upcased_email = @attr[:email].upcase
+    User.create!(@attr.merge(:email => upcased_email))
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
 
-describe "password validations" do
+  describe "password validations" do
 
     it "should require a password" do
       User.new(@attr.merge(:password => "", :password_confirmation => "")).
@@ -78,7 +78,8 @@ describe "password validations" do
       User.new(hash).should_not be_valid
     end
   end
-describe "password encryption" do
+
+  describe "password encryption" do
 
     before(:each) do
       @user = User.create!(@attr)
@@ -91,5 +92,35 @@ describe "password encryption" do
     it "should set the encrypted password" do
       @user.encrypted_password.should_not be_blank
     end
+    
+    describe "has_password? method" do
+
+      it "should be true if the passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+
+      it "should be false if the passwords don't match" do
+        @user.has_password?("invalid").should be_false
+      end
+    end
+
+      describe "authenticate method" do
+
+        it "should return nil on email/password mismatch" do
+          wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
+          wrong_password_user.should be_nil
+        end
+
+        it "should return nil for an email address with no user" do
+          nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
+          nonexistent_user.should be_nil
+        end
+
+        it "should return the user on email/password match" do
+          matching_user = User.authenticate(@attr[:email], @attr[:password])
+          matching_user.should == @user
+        end
+
+      end
+    end
   end
-end
