6ce09af9d7225bd99dbd9b8152c7b69458afeb6d
jeric <jericbello@gmail.com>
2010-09-06 14:42:17 +0800

    revised sign up to sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ require 'digest'
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
-
+  
   EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates_presence_of :name, :email
